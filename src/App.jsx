fa5443ca7b5779732ecf9439c67b3fdee8f3c487
Salil Khanna <salilkhannawork@gmail.com>
2022-01-06 16:04:08 -0500

    fixed grammar and removed plug

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import Home from './components/Home'
-import Footer from './components/Footer'
 
 //the main gateway to the site, with different routes to facilitate navigation and the different pages
 const App = () => {
@@ -9,9 +8,7 @@ const App = () => {
     return (
         <div>
             <Home />
-                    
 
-            <Footer />
         </div>
     )
 }
