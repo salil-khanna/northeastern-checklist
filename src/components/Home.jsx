5ff3b808cc74634ac0265ef1d21c0bd4fd09bf86
Salil Khanna <67717864+salil-khanna@users.noreply.github.com>
2023-04-19 01:33:27 -0700

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -13,7 +13,7 @@ const Home = () => {
     // load up useEffect and initialize number of Takers
 
     //useFetch hook to get all players from the api
-    let heroku = "https://northeastern-checklist.onrender.com/results/"
+    const heroku = process.env.API_LINK;
     const { loading, error, data } = useFetch(heroku)
     useEffect(() => {
         //show loading if the data is still loading
