63a1fed2ac39597215157e6195f9e0ecc2fd1043
Salil Khanna <67717864+salil-khanna@users.noreply.github.com>
2023-04-19 01:42:48 -0700

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -13,7 +13,8 @@ const Home = () => {
     // load up useEffect and initialize number of Takers
 
     //useFetch hook to get all players from the api
-    const heroku = `${process.env.API_LINK}`;
+    const heroku = process.env.API_LINK;
+    console.log(heroku);
     const { loading, error, data } = useFetch(heroku)
     useEffect(() => {
         //show loading if the data is still loading
